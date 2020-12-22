af3faaa4a0ecd23127a459941cf1bc62e7398848
naing-lin-oo <nainglinoo19497@gmail.com>
2020-12-22 14:28:03 +0630

    9-pagination

--- index_old.php
+++ index_old.php
@@ -16,25 +16,7 @@
                     <small>Secondary Text</small>
                 </h1>
 <?php
-    $per_page = 5;
-    if(isset($_GET['page'])){
-        $page = $_GET['page'];
-    } else {
-        $page = "";
-    }
-
-    if($page == "" || $page == 1) {
-        $page_1 = 0;
-    } else {
-        $page_1 = ($page * $per_page) - $per_page;
-    }
-
-    $post_query_count = "SELECT * FROM posts";
-    $find_count_query = mysqli_query($connection, $post_query_count);
-    $count = mysqli_num_rows($find_count_query);
-    $count = ceil($count/$per_page);
-
-    $query = "SELECT * FROM posts LIMIT $page_1, $per_page";
+    $query = "SELECT * FROM posts";
     $select_all_posts_query = mysqli_query($connection, $query);
     while($row = mysqli_fetch_assoc($select_all_posts_query)) {
         $post_id = $row['post_id'];
@@ -59,7 +41,7 @@
                 <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
                 <hr>
                 <a href="post.php?p_id=<?php echo $post_id;?>">
-                    <img width="100%" height="300px" src="images/<?php echo $post_image;?>" alt="">
+                    <img class="img-responsive" src="images/<?php echo $post_image;?>" alt="">
                 </a>
                 <hr>
                 <p><?php echo $post_content; ?></p>
@@ -80,17 +62,6 @@
         <!-- /.row -->
 
         <hr>
-        <ul class="pager">
-<?php
-    for($i=1; $i <= $count; $i++) {
-        if($i == $page) {
-            echo "<li><a class='active_link' href='index.php?page={$i}'>{$i}</a></li>";
-        } else {
-            echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
-        }
-    }
-?>
-        </ul>
 
         <!-- Footer -->
 <?php require('includes/footer.php'); ?>
